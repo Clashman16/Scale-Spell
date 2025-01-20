df881dd25977a584dd761d899a0f55f2011df6aa
Lilian Sananikone <lilian.sananikone@laposte.net>
2025-01-20 14:47:07 +0100

    Add Spell Origin game object in player prefab and draw the spell line dynamicly

--- Assets/Scripts/Managers/ScalerManager.cs
+++ Assets/Scripts/Managers/ScalerManager.cs
@@ -1,11 +1,12 @@
-using Behaviours.UI;
 using UnityEngine;
+using Behaviours.UI;
 
 namespace Managers
 {
    public class ScalerManager
    {
       private GameObject m_scalerGameObject;
+      private Transform m_spellOrigin;
 
       private Vector3 m_previousMousePosition;
       public Vector3 PreviousMousePosition
@@ -25,7 +26,24 @@ namespace Managers
          {
             m_isRescaling = value;
             MapManagerSingleton.GetInstance().Player.GetAnimator().SetBool("IsSpelling", m_isRescaling);
+
+            if(!m_isRescaling)
+            {
+               LineRenderer l_spellRenderer = m_spellOrigin.GetComponent<LineRenderer>();
+               l_spellRenderer.positionCount = 0;
+            }
          }
+      }
+
+      public ScalerManager()
+      {
+         LineRenderer l_spellRenderer = MapManagerSingleton.GetInstance().Player.GetComponentInChildren<LineRenderer>();
+         l_spellRenderer.material = new Material(Shader.Find("Custom/DashedLine"));
+         l_spellRenderer.material.SetColor("_MainColor", Color.gray);
+         l_spellRenderer.startWidth = 0.1f;
+         l_spellRenderer.endWidth = 0.1f;
+
+         m_spellOrigin = l_spellRenderer.transform;
       }
 
       public void DrawScaler(SpriteRenderer l_spriteRenderer)
@@ -33,7 +51,7 @@ namespace Managers
          m_scalerGameObject = new GameObject("Scaler");
          m_scalerGameObject.AddComponent<LineRenderer>();
          ScalerBehaviour l_scaler = m_scalerGameObject.AddComponent<ScalerBehaviour>();
-         l_scaler.Init(l_spriteRenderer);
+         l_scaler.Init(l_spriteRenderer, m_spellOrigin);
       }
 
       public void EraseScaler()
@@ -49,6 +67,12 @@ namespace Managers
          if(m_scalerGameObject != null)
          {
             m_scalerGameObject.GetComponent<LineRenderer>().material.SetColor("_MainColor", p_color);
+         }
+
+         LineRenderer l_spellRenderer = m_spellOrigin.GetComponent<LineRenderer>();
+         if (l_spellRenderer != null)
+         {
+            l_spellRenderer.material.SetColor("_MainColor", p_color);
          }
       }
    }
--- Assets/Scripts/UI/ScalerBehaviour.cs
+++ Assets/Scripts/UI/ScalerBehaviour.cs
@@ -8,18 +8,22 @@ namespace Behaviours
       {
          private SpriteRenderer m_target;
          private LineRenderer m_lineRenderer;
+         private Transform m_spellOrigin;
 
-         public void Init(SpriteRenderer p_target)
+         public void Init(SpriteRenderer p_target, Transform p_spellOrigin)
          {
             m_target = p_target;
 
             m_lineRenderer = GetComponent<LineRenderer>();
 
             m_lineRenderer.material = new Material(Shader.Find("Custom/DashedLine"));
-            m_lineRenderer.material.color = Color.gray;
+            m_lineRenderer.material.SetColor("_MainColor", Color.gray);
 
             m_lineRenderer.startWidth = 0.1f;
             m_lineRenderer.endWidth = 0.1f;
+
+            m_spellOrigin = p_spellOrigin;
+            m_spellOrigin.GetComponent<LineRenderer>().material.SetColor("_MainColor", Color.gray); ;
 
             UpdateScalerPosition();
          }
@@ -40,8 +44,14 @@ namespace Behaviours
                m_lineRenderer.positionCount = l_corners.Length;
                m_lineRenderer.SetPositions(l_corners);
                m_lineRenderer.sortingLayerName = "Scaler";
-            }
 
+               LineRenderer l_spellRenderer = m_spellOrigin.GetComponent<LineRenderer>();
+               l_spellRenderer.positionCount = 2;
+               l_spellRenderer.SetPosition(0, m_spellOrigin.position);
+
+               Vector3 l_spellEnd = (l_corners[3] + l_corners[4]) / 2f;
+               l_spellRenderer.SetPosition(1, l_spellEnd);
+            }
          }
 
          void Update()
